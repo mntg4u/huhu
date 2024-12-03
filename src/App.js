19f0fb1e44990beed1e2d07eca8a3d988243d114
MN - TG <mntg3509@gmail.com>
2024-12-03 12:21:36 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BotTokenForm from './BotTokenForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Serverless Telegram Bot Hosting</h1>
+      <BotTokenForm />
+    </div>
+  );
+}
+
+export default App;
